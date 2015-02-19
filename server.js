2c40212f105345e6a47d4365634e0ad51dce8c4a
gregaubs <gaj.aubert@gmail.com>
2015-02-19 11:26:05 +0000

    server ammed

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ var Twitter = require('twitter');
 var home = fs.readFileSync("./home.html").toString();
 var port = process.env.PORT || 8899;
 
-var client = new Twitter({
-  consumer_key: 'oBki8VkE6KxxgUuVhZhgqgYBc',
-  consumer_secret: 'rNRyagetrcQLX0FCgyy21Br7abKh3RGgfSi2aYmIj27k8a5BaK',
-  access_token_key: '1613170598-lGarGpI41n4e0jRGdh04BusqceTxzPPvc7Quysy',
-  access_token_secret: 'POkTkYf9LArOvS51b9tBe4VR1RoFAP0XQiNkQtpfo3bet'
-});
+// var client = new Twitter({
+//   consumer_key: 'oBki8VkE6KxxgUuVhZhgqgYBc',
+//   consumer_secret: 'rNRyagetrcQLX0FCgyy21Br7abKh3RGgfSi2aYmIj27k8a5BaK',
+//   access_token_key: '1613170598-lGarGpI41n4e0jRGdh04BusqceTxzPPvc7Quysy',
+//   access_token_secret: 'POkTkYf9LArOvS51b9tBe4VR1RoFAP0XQiNkQtpfo3bet'
+// });
 
 
 function start(){
@@ -20,12 +20,12 @@ function start(){
 		var pathname = url.parse(request.url).pathname; 
 		// pathnameChars = pathname.split('');
 		// console.log('server.js  pathname is ' + pathname)
-		if(request.url.indexOf('.css') === -1 && request.url.indexOf('.js') === -1 ) {
-			console.log(" - - - - - - request :");
-			console.log(request);
-			console.log(" - - - - - - ");
-		// console.dir(request);
-		}
+		// if(request.url.indexOf('.css') === -1 && request.url.indexOf('.js') === -1 ) {
+		// 	console.log(" - - - - - - request :");
+		// 	console.log(request);
+		// 	console.log(" - - - - - - ");
+		// // console.dir(request);
+		// }
 		console.log(">> "+pathname);
 		if(pathname==="/"){
 			response.writeHead(200, {'Content-Type': 'text/html'});
@@ -40,9 +40,7 @@ function start(){
 			var pathname = pathname.split('getInsta/')[1].replace('/','');
 
 			api.getInsta(response, pathname);
-		}
-		else	
-		{
+		} else	{
 			ecstatic (request, response);      
 			//ecstatic handles all the files contained in the public folder. 
 		}
